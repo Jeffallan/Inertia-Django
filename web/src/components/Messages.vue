3c462dc586e46e2dbaaa2d25494f84b713c84e93
Raven Security <jeff@ravensecurity.net>
2024-02-11 14:28:06 -0600

    adds styling to messaging framework.

--- web/src/components/Messages.vue
+++ web/src/components/Messages.vue
@@ -1,11 +1,25 @@
 <script setup>
 
-defineProps({messages: Array})
+let props = defineProps({messages: Array})
+
+function messagePayload(){
+    if (props.messages.length > 0){
+    let arr = []
+    for (let i = 0; i < props.messages.length; i++) {
+        let message = props.messages[i]
+        arr.push(JSON.parse(message.replace(/'/g, '"')))
+}
+return arr
+}
+}
 
 </script>
 
 <template>
-
- <h2 v-for="m in messages">{{ m }}</h2>
-
+ <v-alert v-for="m in messagePayload()" closable 
+                                        :text="m.message"
+                                        :type="m.class"
+                                        :title="m.title"
+                                        variant="tonal" 
+                                        />
 </template>
